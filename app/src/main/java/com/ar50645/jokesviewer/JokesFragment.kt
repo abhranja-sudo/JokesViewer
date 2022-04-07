7dc3386a032c68e290339142b651217fd472b8ad
ranjan <ar50645@gmail.com>
2022-04-06 22:42:34 -0700

    Vertical Scroll

--- app/src/main/java/com/ar50645/jokesviewer/JokesFragment.kt
+++ app/src/main/java/com/ar50645/jokesviewer/JokesFragment.kt
@@ -1,16 +1,17 @@
 package com.ar50645.jokesviewer
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlin.math.abs
 
 
 class JokesFragment: Fragment() {
-    private var initTouchX = 0
+    private var initTouchY = 0
     private var initJokeArrayIndex = 0
 
     private lateinit var jokeTextView: TextView
@@ -25,6 +26,32 @@ class JokesFragment: Fragment() {
         val parentView = inflater.inflate(R.layout.fragment_joke, container, false)
         jokeTextView = parentView.findViewById(R.id.jokeTextView)
         jokesArray = getResources().getStringArray((R.array.jokes))
+
+        // Moving finger up or down changes jokes
+        parentView.setOnTouchListener { v, event ->
+            var returnVal = true
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    initTouchY = event.y.toInt()
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val y = event.y.toInt()
+
+                    // See if movement is at least 20 pixels
+                    if (abs(y - initTouchY) >= 40) {
+                        if (y > initTouchY) {
+                            updateJoke()
+                        } else {
+                            updateJoke()
+                        }
+                        initTouchY = y
+                    }
+                }
+                else -> returnVal = false
+            }
+            returnVal
+        }
+
         return parentView
     }
 
