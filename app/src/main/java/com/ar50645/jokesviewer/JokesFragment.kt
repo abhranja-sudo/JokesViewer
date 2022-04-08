1870f788a1614807dc33986d4a738b8c9e8903ae
ranjan <ar50645@gmail.com>
2022-04-08 14:12:30 -0700

    context_menu

--- app/src/main/java/com/ar50645/jokesviewer/JokesFragment.kt
+++ app/src/main/java/com/ar50645/jokesviewer/JokesFragment.kt
@@ -1,18 +1,16 @@
 package com.ar50645.jokesviewer
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlin.math.abs
 
 
 class JokesFragment: Fragment() {
     private var initTouchY = 0
-    private var initJokeArrayIndex = 0
+    private var jokeIndex = 0
 
     private lateinit var jokeTextView: TextView
     var jokesArray: Array<String> = emptyArray()
@@ -21,11 +19,22 @@ class JokesFragment: Fragment() {
         fun newInstance() = JokesFragment()
     }
 
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        activity?.menuInflater?.inflate(R.menu.context_menu, menu)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val parentView = inflater.inflate(R.layout.fragment_joke, container, false)
         jokeTextView = parentView.findViewById(R.id.jokeTextView)
         jokesArray = getResources().getStringArray((R.array.jokes))
+
+        registerForContextMenu(jokeTextView)
 
         // Moving finger up or down changes jokes
         parentView.setOnTouchListener { v, event ->
@@ -37,11 +46,13 @@ class JokesFragment: Fragment() {
                 MotionEvent.ACTION_MOVE -> {
                     val y = event.y.toInt()
 
-                    // See if movement is at least 20 pixels
-                    if (abs(y - initTouchY) >= 40) {
+                    // See if movement is at least 500 pixels
+                    if (abs(y - initTouchY) >= 500) {
                         if (y > initTouchY) {
+                            jokeIndex--
                             updateJoke()
                         } else {
+                            jokeIndex++
                             updateJoke()
                         }
                         initTouchY = y
@@ -60,13 +71,31 @@ class JokesFragment: Fragment() {
         if (savedInstanceState == null) {
             updateJoke()
         } else {
-            //TODO: retrieve the value if thats saved
+            updateJoke()
         }
+    }
 
-
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.next -> {
+                jokeIndex++
+                updateJoke()
+                true
+            }
+            R.id.prev -> {
+                jokeIndex--
+                updateJoke()
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
     }
 
     private fun updateJoke() {
-        jokeTextView.text = jokesArray.get(initJokeArrayIndex++)
+        if(jokeIndex < 0 || jokeIndex >= jokesArray.size) {
+            Toast.makeText(activity, "no more jokes to display", Toast.LENGTH_SHORT).show()
+            return
+        }
+        jokeTextView.text = jokesArray.get(jokeIndex)
     }
 }
--- app/src/main/java/com/ar50645/jokesviewer/MainActivity.kt
+++ app/src/main/java/com/ar50645/jokesviewer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ar50645.jokesviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
